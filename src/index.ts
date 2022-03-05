5b2cf4b4ccc6479dce5bda6b92be86172eda9e06
Italo Di Renzo <italodirenzo93@gmail.com>
2022-03-05 18:37:08 -0500

    Create data set and test case for User Story 1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import { platform } from 'os';
 
+/**
+ * Transforms an encoded account number into a decimal representation
+ * @param str Encoded account number to parse
+ * @returns Decoded account number in decimal form
+ */
+export function decodeAccountNumber(str: string): string {
+  return '';
+}
+
 console.log(`Running on ${platform()}`);
--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,3 +1,10 @@
-it('works', () => {
-  expect(true).toEqual(true);
+import { decodeAccountNumber } from '../src';
+import { useCase1 } from './fixtures';
+
+describe('decodeAccountNumber', () => {
+  it.each(useCase1)(
+    'can translate strings into numbers',
+    (encoded: string, decoded: string) =>
+      expect(decodeAccountNumber(encoded)).toEqual(decoded)
+  );
 });
