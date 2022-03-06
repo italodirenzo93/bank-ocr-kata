c70830546bb73dc64c8cca42c7d3c0e016912f56
Italo Di Renzo <italodirenzo93@gmail.com>
2022-03-05 19:13:02 -0500

    Assert decodeAccountNumber throws on invalid input

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import { platform } from 'os';
 
 /**
  * Transforms an encoded account number into a decimal representation
- * @param str Encoded account number to parse
+ * @param encoded Encoded account number to parse
  * @returns Decoded account number in decimal form
  */
-export function decodeAccountNumber(str: string): string {
+export function decodeAccountNumber(encoded: string): string {
+  const lines = encoded.split('\n').filter((line) => line !== '');
+
+  if (lines.length !== 3)
+    throw new Error(
+      'Encoded account number must be exactly 3 lines of 27 characters each.'
+    );
+
+  for (let line = 0; line < 3; line++) {
+    for (let character = 0; character < 27; character += 3) {}
+  }
+
   return '';
 }
 
--- test/example.spec.ts
+++ test/example.spec.ts
@@ -3,8 +3,27 @@ import { useCase1 } from './fixtures';
 
 describe('decodeAccountNumber', () => {
   it.each(useCase1)(
-    'can translate strings into numbers',
-    (encoded: string, decoded: string) =>
-      expect(decodeAccountNumber(encoded)).toEqual(decoded)
+    'can translate encoded account numbers into decimal format',
+    (encoded: string, decoded: string) => {
+      expect(decodeAccountNumber(encoded)).toEqual(decoded);
+    }
   );
+
+  it('throws when encoded number is more than 3 lines', () => {
+    const encoded = `
+          kf
+               test
+    fl
+    `;
+
+    expect(() => decodeAccountNumber(encoded)).toThrowError();
+  });
+
+  it('throws when encoded number is less than 3 lines', () => {
+    const encoded = `
+          kf
+    `;
+
+    expect(() => decodeAccountNumber(encoded)).toThrowError();
+  });
 });
